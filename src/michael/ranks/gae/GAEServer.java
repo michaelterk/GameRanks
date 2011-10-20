74d98c2d71b128b90e30fa2d3827bf20ab24f82a
michaelterk <michaelter@yahoo.com>
2011-10-19 22:40:17 -0700

    fixed locations in ant file

--- src/michael/ranks/gae/GAEServer.java
+++ src/michael/ranks/gae/GAEServer.java
@@ -2,7 +2,7 @@ package michael.ranks.gae;
 
 public class GAEServer {
 	public static void main(String... args) throws Exception {
-		String[] warArgs = {"com.google.appengine.tools.development.DevAppServerMain", "--port=8080","c:/dev/workspace/chalanga/Foosball/gae-war/"};
+		String[] warArgs = {"com.google.appengine.tools.development.DevAppServerMain", "--port=8080","./gae-war/"};
 		com.google.appengine.tools.KickStart.main(warArgs);
 	}
 }
